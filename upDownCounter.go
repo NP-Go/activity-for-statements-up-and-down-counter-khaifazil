8b456e31acddea3fc55061ffdc9eead879bb937c
khaifazil <khairal.fazil@gmail.com>
2022-03-31 20:42:51 +0800

    completed activity

--- upDownCounter.go
+++ upDownCounter.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert code here
+	var num1 int
+	var num2 int
+
+	fmt.Println("Enter first number: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter second number: ")
+	fmt.Scanln(&num2)
+
+	for i := num1; i <= num2; i++ {
+		fmt.Println(i)
+	}
+
+	for i := num2; i >= num1; i-- {
+		fmt.Println(i)
+	}
 }
